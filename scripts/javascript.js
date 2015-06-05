2a10038bd7e635a51d1fbcd1d09804318664fc0d
jfetter <jillian.fetter@gmail.com>
2015-06-04 20:06:47 -0700

    added function to scroll through captions beside my name

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -7,3 +7,27 @@ $(document).ready(function(){
         $('div').fadeTo("fast",0.5)
     });
 });
+
+
+var captions = ["The mewing kitten video of web developers", "i.e. adorable and always in demand","They call me Jife pop 'cause I be pushin' and popin in arrays like a pie tin full of oil and corn...", "and also 'cause its the first 2 letters of my first and last name" ];
+var captionNumber = 0;
+var captionLength = captions.length -1;
+
+function changeCaption(x)
+{
+  captionNumber += x;
+  if (captionNumber > captionLength){
+    captionNumber =0;
+  }
+  if (captionNumber < 0){
+    captionNumber = captionLength;
+  }
+
+document.getElementById("caption").innerHTML = captions[captionNumber];
+
+return false;
+}
+
+function autoRun(){
+  setInterval("changeCaption(1)", 5000);
+};
